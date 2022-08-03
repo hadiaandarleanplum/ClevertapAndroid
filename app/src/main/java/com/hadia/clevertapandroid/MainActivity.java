f1dcca4513b8ac9456ebcfcd2810b0eaa637b957
hadiaandarleanplum <hadia.andar@leanplum.com>
2022-08-03 15:35:08 -0700

    tried to add push event

--- app/src/main/java/com/hadia/clevertapandroid/MainActivity.java
+++ app/src/main/java/com/hadia/clevertapandroid/MainActivity.java
@@ -93,7 +93,14 @@ public class MainActivity extends AppCompatActivity {
         clevertapDefaultInstance.pushFcmRegistrationId(fcmRegId,true);
 
         //EVENTS
-        // event without properties
-        //CleverTapAPI.pushEvent("Product viewed");
+        Button btnAddedtoCart = findViewById(R.id.btnAddedtoCart);
+        btnAddedtoCart.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            public void onClick (View v){
+                CleverTapAPI.pushEvent("Added to Cart");
+            }
+        });
     }
+
+
 }
